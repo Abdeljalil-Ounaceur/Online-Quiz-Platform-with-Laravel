013ffff843912c24f6970de5fff031f9114b72de
Ounaceur <abdeljalil.ounaceur2003@gmail.com>
2023-04-02 18:21:31 +0000

    deleted uselesse stuff from  routes and PagesController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -28,29 +28,4 @@ class PageController extends Controller
       'users' => User::all()
     ]);
   }
-
-  public function vr()
-  {
-    return view("pages.virtual-reality");
-  }
-
-  public function rtl()
-  {
-    return view("pages.rtl");
-  }
-
-  public function profile()
-  {
-    return view("pages.profile-static");
-  }
-
-  public function signin()
-  {
-    return view("pages.sign-in-static");
-  }
-
-  public function signup()
-  {
-    return view("pages.sign-up-static");
-  }
 }
